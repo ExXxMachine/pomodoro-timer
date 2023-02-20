1cc64bf3dbf98b04d884a9b2f2d1ece4fdec2754
ExXx <barmin_2003@bk.ru>
2023-02-20 23:39:12 +0600

    1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from "react";
+import Menu from "./components/Menu/Menu"
+import Title from "./components/Title/Title"
+import Timer from "./components/Timer/Timer"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Menu/>
+      <div className={"content__block"}>
+        <Title/>
+        <Timer/>
+          <div className={"red__line"}></div>
+      </div>
     </div>
   );
 }
